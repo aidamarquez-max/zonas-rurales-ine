b21ba3c4226b5ed879a063865255ea03de573aa1
aidamarquez-max <aida.marquez@cruzroja.es>
2026-06-03 14:55:45 +0200

    deploy file

--- zonas-rurales-v5-final/vite.config.js
+++ zonas-rurales-v5-final/vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // No proxy needed - app is 100% offline
 export default defineConfig({
+   base: "/zonas-rurales-v5-finall/",
   plugins: [svelte()],
 })
